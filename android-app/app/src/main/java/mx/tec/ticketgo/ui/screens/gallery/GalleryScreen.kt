25dc431c5fe8a0ce39bf45f6c996ff0628dae63b
Angel <A01425478@tec.mx>
2025-09-26 10:05:14 -0600

    Gallery screen added

--- android-app/app/src/main/java/mx/tec/ticketgo/ui/screens/gallery/GalleryScreen.kt
+++ android-app/app/src/main/java/mx/tec/ticketgo/ui/screens/gallery/GalleryScreen.kt
@@ -4,10 +4,8 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.unit.dp
 import mx.tec.ticketgo.ui.components.GalleryItem
 
